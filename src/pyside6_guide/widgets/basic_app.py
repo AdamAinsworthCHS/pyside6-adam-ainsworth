6a8c7b56fae1708a9242a11c4d66db2ac43d53bd
AdamAinsworthCHS <147109448+AdamAinsworthCHS@users.noreply.github.com>
2025-12-11 08:44:38 -0800

    added location input and clear button to basic_app

--- src/pyside6_guide/widgets/basic_app.py
+++ src/pyside6_guide/widgets/basic_app.py
@@ -31,9 +31,17 @@ class MainWindow(QMainWindow):
         self.username_input = QLineEdit()
         self.username_input.setPlaceholderText("Enter your name here.")
 
+        # input field to add location
+        self.location_input = QLineEdit()
+        self.location_input.setPlaceholderText("Enter your location here.")
+
         # TODO: add a push button to greet user
         greeting_button = QPushButton("Get Greeting")
         greeting_button.clicked.connect(self.get_greeting)
+
+        # push button that clears the app
+        clear_button = QPushButton("Clear")
+        clear_button.clicked.connect(self.clear_app)
 
         # TODO: add a label to greet user
         greeting = "Enter your name, then press the get greeting button"
@@ -48,12 +56,15 @@ class MainWindow(QMainWindow):
             * Add an HBox for each input that contains a label (on the left)
               and input box so they line up side by side
                 - then add that Hbox where you initially added the input
+                - so basically instead of QVBoxLayout use HBox one.
         """
 
         # add widgets & layouts to main layout
         layout.addWidget(title_label)
         layout.addWidget(self.username_input)
+        layout.addWidget(self.location_input)
         layout.addWidget(greeting_button)
+        layout.addWidget(clear_button)
         layout.addWidget(self.greeting_label)
 
         # [OPTIONAL] Add a stretch to move everything up
@@ -68,7 +79,15 @@ class MainWindow(QMainWindow):
     def get_greeting(self):
         """get the user's name and greet the user by name"""
         username = self.username_input.text()
-        greeting = f"Hello, {username} what is up?"
+        location = self.location_input.text()
+        greeting = f"Hello, {username} from {location} what is up?"
+        self.greeting_label.setText(greeting)
+    
+    def clear_app(self):
+        """clear the inputs and outputs"""
+        self.username_input.setText("")
+        self.location_input.setText("")
+        greeting = "Enter your name, then press the get greeting button"
         self.greeting_label.setText(greeting)
         
 
